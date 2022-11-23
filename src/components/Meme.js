bf5f80dc1bd8266b5675e78735b75c59bcb15502
Swati <swatic019@gmail.com>
2022-11-23 22:53:41 +0530

    memes data created

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,21 @@
 import React from "react";
+import memesData from "../memesData";
 
 function Meme() {
+function getMemeImage(){
+  let memesArray = memesData.data.memes;
+  console.log(memesArray);
+
+}
+
+
+
   return (
     <main>
       <form className="form">
         <input type="text" className="form-input" placeholder="top-text"></input>
         <input type="text" className="form-input" placeholder="bottom-text"></input>
-        <button className="form-button">Get a new meme image 🖼</button>
+        <button className="form-button" onClick={getMemeImage}>Get a new meme image 🖼</button>
       </form>
     </main>
   );
