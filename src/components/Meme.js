ab7825820e6eddee05f0f7fc3b25fe8e0f2040f8
Swati <swatic019@gmail.com>
2022-11-26 17:22:22 +0530

    rendered images enter the commit message for your changes. Lines starting

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,21 +2,33 @@ import React from "react";
 import memesData from "../memesData";
 
 function Meme() {
-function getMemeImage(){
-  let memesArray = memesData.data.memes;
-  console.log(memesArray);
+const [memeImage, setMemeImage] = React.useState("");
 
-}
-
-
+  function getMemeImage() {
+    let memesArray = memesData.data.memes;
+    let randomNumber = Math.floor(Math.random() * memesArray.length);
+   
+    setMemeImage(memesArray[randomNumber].url)
+  }
 
   return (
     <main>
-      <form className="form">
-        <input type="text" className="form-input" placeholder="top-text"></input>
-        <input type="text" className="form-input" placeholder="bottom-text"></input>
-        <button className="form-button" onClick={getMemeImage}>Get a new meme image 🖼</button>
-      </form>
+      <div className="form">
+        <input
+          type="text"
+          className="form-input"
+          placeholder="top-text"
+        ></input>
+        <input
+          type="text"
+          className="form-input"
+          placeholder="bottom-text"
+        ></input>
+        <button className="form-button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+        <img src={memeImage} alt="meme-img" className="meme-image"></img>
     </main>
   );
 }
