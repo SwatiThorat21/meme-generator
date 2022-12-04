b7baa5f8ae6351a1a147e3a9584690dd8e5e2180
Swati <swatic019@gmail.com>
2022-12-04 08:51:35 +0530

    text renders on page

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -6,7 +6,7 @@ function Meme() {
 
   const [meme, setMeme] = React.useState({
     topText: "",
-    bottomTxt: "",
+    bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
@@ -16,12 +16,18 @@ function Meme() {
     let memesArray = allMemeImages.data.memes;
     let randomNumber = Math.floor(Math.random() * memesArray.length);
     let url = memesArray[randomNumber].url;
-    setMeme((prevMeme) => {
-      return {
-        ...prevMeme,
-        randomImage: url,
-      };
-    });
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function handleChange(event) {
+    const {name, value} = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
   }
 
   return (
@@ -31,11 +37,17 @@ function Meme() {
           type="text"
           className="form-input"
           placeholder="top-text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
         ></input>
         <input
           type="text"
           className="form-input"
           placeholder="bottom-text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         ></input>
         <button className="form-button" onClick={getMemeImage}>
           Get a new meme image 🖼
@@ -43,8 +55,8 @@ function Meme() {
       </div>
       <div className="meme">
         <img src={meme.randomImage} alt="meme-img" className="meme-image"></img>
-        <h2 className="meme--text top">One does not simply</h2>
-        <h2 className="meme--text bottom">Walk into Mordor</h2>
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );
